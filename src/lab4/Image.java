778958d94654b34705d76e4c73ac3ad51e91f5ee
Diana Anton <diana.anton00@e-uvt.ro>
2021-10-19 08:10:44 +0300

    Files for lab4

---
+++ src/lab4/Image.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package lab4;
+
+/**
+ * @author Diana Anton
+ *
+ */
+public class Image implements Element {
+    private String imageName;
+
+    public Image(String imageName) {
+        this.imageName = imageName;
+    }
+    
+    public void print(){
+        System.out.println("Image with name: " + imageName);
+    }
+
+	@Override
+	public void add(Element e) {
+		// TODO Auto-generated method stub
+		// nu e nevoie sa le implementam ca astea sunt frunze
+	}
+
+	@Override
+	public Element get(int poz) {
+		// TODO Auto-generated method stub
+		// nu e nevoie sa le implementam ca astea sunt frunze
+		return null;
+	}
+
+	@Override
+	public void remove(Element e) {
+		// TODO Auto-generated method stub
+		// nu e nevoie sa le implementam ca astea sunt frunze
+	}
+
+	@Override
+	public boolean find(Element e) {
+		// TODO Auto-generated method stub
+		if (!(e instanceof Image)) {
+			return false;
+		}
+		else {
+			return ((Image) e).imageName.equals(this.imageName);
+		}
+	}
+}
